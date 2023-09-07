baa6d8175435f5df23dc0b966368bad920688694
Arina <arinaligit@gmail.com>
2023-09-07 11:05:23 +0300

    first commit

---
+++ FactsApp/FactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactsAppApp.swift
+//  FactsApp
+//
+//  Created by Arina on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
