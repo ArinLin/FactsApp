61600a923f7282b7617fbc369b90bd3e0bf5990d
Arina <arinaligit@gmail.com>
2023-09-07 15:45:44 +0300

    add FirstLaunch check

--- FactsApp/FactsAppApp.swift
+++ FactsApp/FactsAppApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct FactsAppApp: App {
+    @AppStorage("isFirstLaunch") var isFirstLaunch: Bool = true
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if isFirstLaunch {
+                ContentView()
+                    .onChange(of: scenePhase) { newPhase in
+                        if newPhase == .inactive {
+                            isFirstLaunch = false
+                        }
+                    }
+            } else {
+                FourthScreen()
+            }
         }
     }
 }
